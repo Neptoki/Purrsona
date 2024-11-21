f6b0f74229bb805994ae56ca4afbf69d61dde1a5
Neptoki <zoedn1911@gmail.com>
2024-11-21 11:28:26 +0000

    holding down jump increases height, also fixed the cat spinning when hitting an object

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -10,6 +10,13 @@ public class PlayerMove : MonoBehaviour
     public Rigidbody2D playerRb;
     public SpriteRenderer spriteRenderer;
     public float jumpForce;
+    public LayerMask groundLayer; // to be able to set objects as ground
+    private bool isGrounded;
+    public float groundCheckCircle;
+    public Transform pawsPosition;
+    public float jumpTime = 0.2f; // time the player's jump can be held before character comes back down
+    public float jumpTimeCounter; // counts how long the jump has left
+    private bool isJumping;
 
     // Start is called before the first frame update
     void Start()
@@ -32,12 +39,36 @@ public class PlayerMove : MonoBehaviour
             spriteRenderer.flipX = false;
         }
 
-        // player jumping, checks to also see if the jump button is pressed
-        if (Input.GetButton("Jump"))
+        // check if player is on ground before allowed to jump again
+        isGrounded = Physics2D.OverlapCircle(pawsPosition.position, groundCheckCircle, groundLayer);
+
+        // player jumping, checks to also see if the jump button is pressed, initates the jump
+        if (isGrounded == true && Input.GetButtonDown("Jump"))
         {
+            isJumping = true;
+            jumpTimeCounter = jumpTime; // timer gets reset with every jump
             playerRb.velocity = Vector2.up * jumpForce;
         }
 
+        // if the jump button is to be held down
+        if (Input.GetButton("Jump") && isJumping == true) // can't jump whilst falling
+        {
+            if(jumpTimeCounter > 0) // if there is time left, can hold jump
+            {
+                playerRb.velocity = Vector2.up * jumpForce;
+                jumpTimeCounter -= Time.deltaTime; // makes the timer count down
+            }
+
+            else
+            {
+                isJumping = false; // if timer runs out or stop pressing jumping
+            }
+        }
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isJumping = false;
+        }
     }
 
     void FixedUpdate()
