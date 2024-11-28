12ffcd1b251b61231eae3c5919d73754a2dc745f
Neptoki <zoedn1911@gmail.com>
2024-11-28 18:54:01 +0000

    halfway through shooting

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public GameObject bullet;
+    public Transform bulletTransform;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition); // moves mouse cusors based on vector3
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ); // gets rotational movement
+
+        if (Input.GetMouseButton(0))
+        {
+            Instantiate(bullet, bulletTransform.position, Quaternion.identity);
+        }
+
+    }
+}
