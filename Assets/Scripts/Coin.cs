93928e6387099635144419d89e3aeb33d9544ab8
Neptoki <zoedn1911@gmail.com>
2024-11-28 10:43:29 +0000

    starting work on getting collectibles and scoring system

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : UICollectible
+{
+
+    public override void Collect()
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
