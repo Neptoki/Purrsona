acd34d6c44276048f8c616f192149972b185bccf
Neptoki <zoedn1911@gmail.com>
2024-11-28 21:03:11 +0000

    bullet code done?

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -26,8 +26,20 @@ public class Shooting : MonoBehaviour
         float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
         transform.rotation = Quaternion.Euler(0, 0, rotZ); // gets rotational movement
 
-        if (Input.GetMouseButton(0))
+        // fire rate and if can fire after firing
+        if (!canFire)
         {
+            timer += Time.deltaTime;
+            if(timer > timeBetweenFiring)
+            {
+                canFire = true;
+                timer = 0;
+            }
+        }
+
+        if (Input.GetMouseButton(0) && canFire)
+        {
+            canFire = false;
             Instantiate(bullet, bulletTransform.position, Quaternion.identity);
         }
 
