33ca4c307f2ec6872043880f383695becbe24df6
Neptoki <zoedn1911@gmail.com>
2024-11-30 19:39:18 +0000

    timere ends the game

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -7,6 +7,8 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI timerText;
     [SerializeField] float remainingTime;
+    public GameOverScreen GameOverScreen;
+
 
     // Update is called once per frame
     void Update()
@@ -20,10 +22,19 @@ public class Timer : MonoBehaviour
         {
             remainingTime = 0;
             timerText.color = Color.red;
+            GameOver();
         }
 
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void GameOver()
+    {
+        if (GameOverScreen != null)
+        {
+            GameOverScreen.Setup(true);
+        }
     }
 }
